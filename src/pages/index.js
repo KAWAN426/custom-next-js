ec90873691359bbb3ce77d9b6ad5dd2253358d5b
CWIN77 <cwstbp0426@gmail.com>
2022-10-28 02:18:45 +0900

    Nextjs to javascript

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export default function Home() {
+  return (
+    <Container>
+      <h1>My Next WebApp</h1>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  width:100vw;
+  min-height:100vh;
+`
